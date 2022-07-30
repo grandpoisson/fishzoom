88575b817381b571efb753d73ce43c6eebc36f54
grandpoisson <grandpoisson@gmail.com>
2022-07-30 13:26:04 +0300

    handle second user

--- pages/room/[id]/join.js
+++ pages/room/[id]/join.js
@@ -1,39 +1,33 @@
 import Script from "next/script";
 import { useRouter } from "next/router";
 
-export default function Room() {
+export default function Join() {
   const router = useRouter();
   const { id } = router.query;
+
   return (
     <>
       <Script
         src="https://unpkg.com/peerjs@1.4.5/dist/peerjs.min.js"
         onLoad={async () => {
-          const peer = new Peer(`room-${id}-first`);
+          const peer = new Peer(`room-${id}-second`);
+
           const localStream = await navigator.mediaDevices.getUserMedia({
             video: true,
             audio: true,
           });
-          document.querySelector("video#local").srcObject = localStream;
-          peer.on("call", (call) => {
-            call.answer(localStream);
 
-            call.on("stream", (remoteStream) => {
-              document.querySelector("video#remote").srcObject = remoteStream;
-            });
+          document.querySelector("video#local").srcObject = localStream;
+
+          const call = peer.call(`room-${id}-first`, localStream);
+          call.on("stream", (remoteStream) => {
+            document.querySelector("video#remote").srcObject = remoteStream;
           });
         }}
       />
-      <h1 className="mt-20 text-center text-3xl uppercase font-black">
-        Chatroom
+      <h1 className="mt-20 mb-20 text-center text-3xl uppercase font-black">
+        Chat invite
       </h1>
-
-      <p className="mt-20 mb-20 text-center text-3xl font-black">
-        Share this link to join the room: <br />
-        <a href={`/room/${id}/join`} className="underline">
-          http://localhost:3000/room/{id}/join
-        </a>
-      </p>
 
       <div className="flex">
         <video id="local" autoPlay playsInline muted></video>
